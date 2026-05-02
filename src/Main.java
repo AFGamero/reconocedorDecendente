41e3c97f3d48a9ba6312a9df83812d2f52101747
Santiago Gonzalez <santiagosaid28@tutamail.com>
2026-05-01 22:22:46 -0500

    feat: it can detect simple '' now

--- src/Main.java
+++ src/Main.java
@@ -30,11 +30,9 @@ import javax.swing.JTextArea;
  */
 public class Main {
 
-    // Mapeo lexema nombre de token para palabras reservadas
     static final Map<String, String> KEYWORDS = new HashMap<>();
 
     static {
-        // Palabras reservadas
         KEYWORDS.put("int",      "INT");
         KEYWORDS.put("main",     "MAIN");
         KEYWORDS.put("void",     "VOID");
@@ -72,7 +70,6 @@ public class Main {
         KEYWORDS.put("union",    "UNION");
     }
 
-    // nombre de token para directivas de preprocesamiento
     static final Map<String, String> DIRECTIVES = new HashMap<>();
 
     static {
@@ -89,7 +86,6 @@ public class Main {
         DIRECTIVES.put("undef",   "PP_UNDEF");
     }
 
-    // Representa un token con tipo y lexema
     static class Token {
         String tipo;
         String valor;
@@ -113,6 +109,7 @@ public class Main {
      *   IN_NUM,acumula digito+ -> INT_NUM
      *   IN_OP, mira un caracter adelante para operadores de dos chars
      *   IN_STRING, acumula caracteres entre comillas dobles con escape
+     *   IN_CHAR, acumula caracteres entre comillas simples con escape
      *   IN_COMMENT, ignora hasta fin de linea (//) o hasta cierre 
      *   IN_PP , lee directiva de preprocesamiento (#...)
      *
@@ -127,22 +124,17 @@ public class Main {
         while (i < n) {
             char c = codigo.charAt(i);
 
-            // START: ignorar espacios
             if (Character.isWhitespace(c)) {
                 i++;
                 continue;
             }
 
-            // IN_PP: directiva de preprocesamiento
             if (c == '#') {
-                i++; // consumir '#'
-                // saltar espacios entre # y el nombre
+                i++;
                 while (i < n && codigo.charAt(i) == ' ') i++;
-                // leer nombre de la directiva
                 int inicio = i;
                 while (i < n && Character.isLetter(codigo.charAt(i))) i++;
                 String nombre = codigo.substring(inicio, i);
-                // leer el resto de la linea como valor
                 int restoInicio = i;
                 while (i < n && codigo.charAt(i) != '\n') i++;
                 String resto = codigo.substring(restoInicio, i).trim();
@@ -152,27 +144,23 @@ public class Main {
                 continue;
             }
 
-            // IN_COMMENT: comentario de linea //
             if (c == '/' && i + 1 < n && codigo.charAt(i + 1) == '/') {
                 while (i < n && codigo.charAt(i) != '\n') i++;
                 continue;
             }
 
-            // IN_COMMENT: comentario de bloque /* ... */
             if (c == '/' && i + 1 < n && codigo.charAt(i + 1) == '*') {
                 i += 2;
                 while (i + 1 < n && !(codigo.charAt(i) == '*' && codigo.charAt(i + 1) == '/')) i++;
-                i += 2; //  */
+                i += 2;
                 continue;
             }
 
-            // IN_STRING: cadena entre comillas dobles
             if (c == '"') {
                 StringBuilder sb = new StringBuilder();
                 sb.append('"');
                 i++;
                 while (i < n && codigo.charAt(i) != '"') {
-                    // manejar secuencias de escape: \n \0 \r \t \\ \v \f \a \" \'
                     if (codigo.charAt(i) == '\\' && i + 1 < n) {
                         sb.append(codigo.charAt(i));
                         sb.append(codigo.charAt(i + 1));
@@ -183,12 +171,31 @@ public class Main {
                     }
                 }
                 sb.append('"');
-                i++; // comilla de cierre
+                i++;
                 tokens.add(new Token("STRING_LITERAL", sb.toString()));
                 continue;
             }
 
-            // IN_ID: inicia con letra o _
+            if (c == '\'') {
+                StringBuilder sb = new StringBuilder();
+                sb.append('\'');
+                i++;
+                while (i < n && codigo.charAt(i) != '\'') {
+                    if (codigo.charAt(i) == '\\' && i + 1 < n) {
+                        sb.append(codigo.charAt(i));
+                        sb.append(codigo.charAt(i + 1));
+                        i += 2;
+                    } else {
+                        sb.append(codigo.charAt(i));
+                        i++;
+                    }
+                }
+                sb.append('\'');
+                i++;
+                tokens.add(new Token("CHAR_LITERAL", sb.toString()));
+                continue;
+            }
+
             if (Character.isLetter(c) || c == '_') {
                 int inicio = i;
                 while (i < n && (Character.isLetterOrDigit(codigo.charAt(i)) || codigo.charAt(i) == '_')) {
@@ -200,7 +207,6 @@ public class Main {
                 continue;
             }
 
-            // IN_NUM: inicia con digito
             if (Character.isDigit(c)) {
                 int inicio = i;
                 while (i < n && Character.isDigit(codigo.charAt(i))) {
@@ -210,7 +216,6 @@ public class Main {
                 continue;
             }
 
-            // IN_OP: simbolos de uno o dos caracteres
             switch (c) {
                 case '{':
                     tokens.add(new Token("LBRACE",   "{"));  i++; break;
@@ -241,7 +246,6 @@ public class Main {
                 case '~':
                     tokens.add(new Token("COMPL_OP", "~"));  i++; break;
 
-                // '-'  MINUS o flecha ->
                 case '-':
                     if (i + 1 < n && codigo.charAt(i + 1) == '>') {
                         tokens.add(new Token("ARROW",  "->")); i += 2;
@@ -250,11 +254,9 @@ public class Main {
                     }
                     break;
 
-                // '/'  DIV_OP 
                 case '/':
                     tokens.add(new Token("DIV_OP",  "/")); i++; break;
 
-                // '&'  AND_OP o ANDAND
                 case '&':
                     if (i + 1 < n && codigo.charAt(i + 1) == '&') {
                         tokens.add(new Token("ANDAND", "&&")); i += 2;
@@ -263,7 +265,6 @@ public class Main {
                     }
                     break;
 
-                // '|'  OR_OP o OROR
                 case '|':
                     if (i + 1 < n && codigo.charAt(i + 1) == '|') {
                         tokens.add(new Token("OROR",  "||")); i += 2;
@@ -272,7 +273,6 @@ public class Main {
                     }
                     break;
 
-                // '!'  NOT_OP o NOTEQ
                 case '!':
                     if (i + 1 < n && codigo.charAt(i + 1) == '=') {
                         tokens.add(new Token("NOTEQ",  "!=")); i += 2;
@@ -281,7 +281,6 @@ public class Main {
                     }
                     break;
 
-                // '='  ASSIGN o EQ
                 case '=':
                     if (i + 1 < n && codigo.charAt(i + 1) == '=') {
                         tokens.add(new Token("EQ",     "==")); i += 2;
@@ -290,7 +289,6 @@ public class Main {
                     }
                     break;
 
-                // '<'  LT, LTEQ o SHL_OP
                 case '<':
                     if (i + 1 < n && codigo.charAt(i + 1) == '<') {
                         tokens.add(new Token("SHL_OP", "<<")); i += 2;
@@ -301,7 +299,6 @@ public class Main {
                     }
                     break;
 
-                // '>'  GT, GTEQ o SHR_OP
                 case '>':
                     if (i + 1 < n && codigo.charAt(i + 1) == '>') {
                         tokens.add(new Token("SHR_OP", ">>")); i += 2;
@@ -313,7 +310,6 @@ public class Main {
                     break;
 
                 default:
-                    // Caracter fuera del lenguaje
                     tokens.add(new Token("ERROR", String.valueOf(c)));
                     i++;
                     break;
@@ -329,7 +325,6 @@ public class Main {
         String ultimoResultado = "";
 
         while (true) {
-            // JOptionPane
             String codigo = JOptionPane.showInputDialog(
                 null,
                 ultimoResultado + "Ingrese el codigo C a escanear:",
@@ -337,16 +332,13 @@ public class Main {
                 JOptionPane.QUESTION_MESSAGE
             );
 
-            //  cierra el programa
             if (codigo == null) break;
 
             System.out.println("\nEntrada:\n" + codigo);
             System.out.println("-".repeat(60));
 
-            // Ejecutar el AFD
             List<Token> tokens = escanear(codigo);
 
-            // Imprimir en consola y acumular para la ventana
             StringBuilder sb = new StringBuilder();
             for (Token t : tokens) {
                 System.out.println(t);
@@ -356,7 +348,6 @@ public class Main {
             System.out.println("-".repeat(60));
             System.out.println("Total de tokens: " + tokens.size());
 
-            // Mostrar resultado en ventana
             JTextArea area = new JTextArea(sb.toString());
             area.setEditable(false);
             area.setFont(new java.awt.Font("Monospaced", java.awt.Font.PLAIN, 13));
